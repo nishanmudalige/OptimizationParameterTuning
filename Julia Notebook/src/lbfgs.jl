43dbbedb1919c4aaf2e3c70f93d993e8f78093d0
Felix Gao <felix.gao@mail.utoronto.ca>
2025-10-08 12:30:52 -0400

    update

--- Julia Notebook/src/lbfgs.jl
+++ Julia Notebook/src/lbfgs.jl
@@ -17,73 +17,75 @@ using JSOSolvers
 
 MAX_TIME = 60.0
 
+function main()
+    nlp = OptimizationProblems.ADNLPProblems.arglina(matrix_free = true) # is one of them
+    meta = OptimizationProblems.meta
+    problem_names = meta[meta.contype .== :unconstrained .&& .!meta.has_bounds .&& meta.nvar .>= 5, :name]; # LBFGS is an algorithm to solve unconstrained problems
+                                                                    # For this example, we select only problems of size up to 10.
+    problems = [Meta.parse("OptimizationProblems.ADNLPProblems.eval($problem)(matrix_free = true)") for problem ∈ problem_names];                                            
+    filename = "../results/lbfgs_matrix_free.csv"
+    mkpath(dirname(filename))
 
-nlp = OptimizationProblems.ADNLPProblems.arglina(matrix_free = true) # is one of them
-meta = OptimizationProblems.meta
-problem_names = meta[meta.contype .== :unconstrained .&& .!meta.has_bounds .&& meta.nvar .>= 5, :name]; # LBFGS is an algorithm to solve unconstrained problems
-                                                                # For this example, we select only problems of size up to 10.
-problems = [Meta.parse("OptimizationProblems.ADNLPProblems.eval($problem)(matrix_free = true)") for problem ∈ problem_names];                                            
-filename = "./results/lbfgs_matrix_free.csv"
-mkpath(dirname(filename))
-
-df = DataFrame(
-                :status => Symbol[],
-                :name => String[],
-                :solver => String[],
-                :mem => Int[],
-                :nvar => Int[],
-                :time => Float64[],
-                :memory => Float64[],
-                :num_iter => Int[],
-                :nvmops => Int[],
-                :neval_obj => Int[],
-                :init_eval_obj_time => Float64[],
-                :neval_grad => Int[],
-                :init_eval_grad_time => Float64[]
-            )
-
-i = 0
-for pb_expr in problems
-    nlp = eval(pb_expr)
-    i += 1
-    param_set = JSOSolvers.LBFGSParameterSet(nlp)
-    r = domain(param_set.mem)
-    problem = nlp.meta.name
-    solver = "LBFGSSolver"
-    for mem in r.lower:r.upper
-        reset!(nlp)
-        println("Running $pb_expr with mem=$mem")
-        try
-            init_eval_obj_time = @elapsed obj(nlp, nlp.meta.x0)
-            init_eval_grad_time = @elapsed grad(nlp, nlp.meta.x0)
-            reset!(nlp)
-            bench = @benchmark JSOSolvers.lbfgs($nlp; mem = $mem, max_time=MAX_TIME)
-            reset!(nlp)
-            stats = JSOSolvers.lbfgs(nlp; mem = mem, max_time=MAX_TIME)
-            push!(df, (
-                        status = stats.status, 
-                        name = problem,
-                        solver = solver,
-                        mem = mem,
-                        nvar = nlp.meta.nvar, 
-                        time = minimum(bench).time / 10^9,
-                        memory = minimum(bench).memory, 
-                        num_iter = stats.iter,
-                        nvmops = stats.solver_specific[:nprod],
-                        neval_obj = nlp.counters.neval_obj,
-                        init_eval_obj_time = init_eval_obj_time,
-                        neval_grad = nlp.counters.neval_grad,
-                        init_eval_grad_time = init_eval_grad_time
-                    )
+    df = DataFrame(
+                    :status => Symbol[],
+                    :name => String[],
+                    :solver => String[],
+                    :mem => Int[],
+                    :nvar => Int[],
+                    :time => Float64[],
+                    :memory => Float64[],
+                    :num_iter => Int[],
+                    :nvmops => Int[],
+                    :neval_obj => Int[],
+                    :init_eval_obj_time => Float64[],
+                    :neval_grad => Int[],
+                    :init_eval_grad_time => Float64[]
                 )
-        catch e
-            @info "Solver failed on $(nlp.meta.name): $e"
-            break
+
+    i = 0
+    for pb_expr in problems
+        nlp = eval(pb_expr)
+        i += 1
+        param_set = JSOSolvers.LBFGSParameterSet(nlp)
+        r = domain(param_set.mem)
+        problem = nlp.meta.name
+        solver = "LBFGSSolver"
+        for mem in r.lower:r.upper
+            reset!(nlp)
+            println("Running $pb_expr with mem=$mem")
+            try
+                init_eval_obj_time = @elapsed obj(nlp, nlp.meta.x0)
+                init_eval_grad_time = @elapsed grad(nlp, nlp.meta.x0)
+                reset!(nlp)
+                bench = @benchmark JSOSolvers.lbfgs($nlp; mem = $mem, max_time=MAX_TIME)
+                reset!(nlp)
+                stats = JSOSolvers.lbfgs(nlp; mem = mem, max_time=MAX_TIME)
+                push!(df, (
+                            status = stats.status, 
+                            name = problem,
+                            solver = solver,
+                            mem = mem,
+                            nvar = nlp.meta.nvar, 
+                            time = minimum(bench).time / 10^9,
+                            memory = minimum(bench).memory, 
+                            num_iter = stats.iter,
+                            nvmops = stats.solver_specific[:nprod],
+                            neval_obj = nlp.counters.neval_obj,
+                            init_eval_obj_time = init_eval_obj_time,
+                            neval_grad = nlp.counters.neval_grad,
+                            init_eval_grad_time = init_eval_grad_time
+                        )
+                    )
+            catch e
+                @info "Solver failed on $(nlp.meta.name): $e"
+                break
+            end
+            CSV.write(
+                filename,
+                DataFrame([last(df)]);
+                append = isfile(filename) && filesize(filename) > 0
+            )
         end
-        CSV.write(
-            filename,
-            DataFrame([last(df)]);
-            append = isfile(filename) && filesize(filename) > 0
-        )
     end
 end
+main()
