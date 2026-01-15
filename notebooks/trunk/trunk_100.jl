0d3e39a52789ab1a8967736e0c98e57c012147e4
Felix Gao <felixgao1226@gmail.com>
2026-01-14 23:20:20 -0500

    add trunk_100.jl and complete_trunk_100.csv

--- notebooks/trunk/trunk_100.jl
+++ notebooks/trunk/trunk_100.jl
@@ -1,5 +1,6 @@
 using Pkg
-Pkg.activate(joinpath(@__DIR__, ".."))
+Pkg.activate(".")
+Pkg.update()
 Pkg.instantiate()
 Pkg.status()
 
@@ -30,19 +31,18 @@ function main()
         Meta.parse("OptimizationProblems.ADNLPProblems.eval($problem)(matrix_free = true)") for problem ∈ problem_names
     ];
 
-    filename = "../results/complete_lbfgs.csv"
+    filename = "./complete_trunk100.csv"
     mkpath(dirname(filename))
 
     df = DataFrame(
         :status => Symbol[],
         :name => String[],
         :solver => String[],
-        :mem => Int[],
         :nvar => Int[],
         :time => Float64[],
         :memory => Float64[],
         :num_iter => Int[],
-        :nvmops => Int[],
+        # :nvmops => Int[],
         :neval_obj => Int[],
         :init_eval_obj_time => Float64[],
         :init_eval_obj_mem => Float64[],
@@ -59,61 +59,56 @@ function main()
         nlp_build_time = @elapsed nlp = eval(pb_expr)
         i += 1
         param_set = JSOSolvers.LBFGSParameterSet(nlp)
-        r = domain(param_set.mem)
         problem = nlp.meta.name
         solver = "LBFGSSolver"
         is_init_run = true
-        for mem = r.lower:r.upper
-            for i = 0:is_init_run
-                reset!(nlp)
-                println("Running $pb_expr with mem=$mem")
-                try
-                    _, init_eval_obj_time, init_eval_obj_mem, _, init_eval_obj_gcstats =
-                        @timed obj(nlp, nlp.meta.x0)
-                    _, init_eval_grad_time, init_eval_grad_mem, _, init_eval_grad_gcstats =
-                        @timed grad(nlp, nlp.meta.x0)
-                    stats, time, memory, _, _ =
-                        @timed JSOSolvers.lbfgs(nlp; mem = mem, max_time = MAX_TIME)
-                    init_eval_obj_alloc =
-                        init_eval_obj_gcstats.poolalloc + init_eval_obj_gcstats.bigalloc
-                    init_eval_grad_alloc =
-                        init_eval_grad_gcstats.poolalloc + init_eval_grad_gcstats.bigalloc
+        for i = 0:is_init_run
+            reset!(nlp)
+            println("Running $pb_expr")
+            try
+                _, init_eval_obj_time, init_eval_obj_mem, _, init_eval_obj_gcstats =
+                    @timed obj(nlp, nlp.meta.x0)
+                _, init_eval_grad_time, init_eval_grad_mem, _, init_eval_grad_gcstats =
+                    @timed grad(nlp, nlp.meta.x0)
+                stats, time, memory, _, _ =
+                    @timed JSOSolvers.trunk(nlp; max_time = MAX_TIME)
+                init_eval_obj_alloc =
+                    init_eval_obj_gcstats.poolalloc + init_eval_obj_gcstats.bigalloc
+                init_eval_grad_alloc =
+                    init_eval_grad_gcstats.poolalloc + init_eval_grad_gcstats.bigalloc
 
-
-                    push!(
-                        df,
-                        (
-                            status = stats.status,
-                            name = problem,
-                            solver = solver,
-                            mem = mem,
-                            nvar = nlp.meta.nvar,
-                            time = time + nlp_build_time,
-                            memory = memory / 1e6,
-                            num_iter = stats.iter,
-                            nvmops = stats.solver_specific[:nprod],
-                            neval_obj = nlp.counters.neval_obj,
-                            init_eval_obj_time = init_eval_obj_time,
-                            init_eval_obj_mem = init_eval_obj_mem / 1e6,
-                            init_eval_obj_alloc = init_eval_obj_alloc,
-                            neval_grad = nlp.counters.neval_grad,
-                            init_eval_grad_time = init_eval_grad_time,
-                            init_eval_grad_mem = init_eval_grad_mem / 1e6,
-                            init_eval_grad_alloc = init_eval_grad_alloc,
-                            is_init_run = is_init_run,
-                        ),
-                    )
-                    is_init_run = false
-                catch e
-                    @info "Solver failed on $(nlp.meta.name): $e"
-                    break
-                end
-                CSV.write(
-                    filename,
-                    DataFrame([last(df)]);
-                    append = isfile(filename) && filesize(filename) > 0,
+                push!(
+                    df,
+                    (
+                        status = stats.status,
+                        name = problem,
+                        solver = solver,
+                        nvar = nlp.meta.nvar,
+                        time = time + nlp_build_time,
+                        memory = memory / 1e6,
+                        num_iter = stats.iter,
+                        # nvmops = stats.solver_specific[:nprod],
+                        neval_obj = nlp.counters.neval_obj,
+                        init_eval_obj_time = init_eval_obj_time,
+                        init_eval_obj_mem = init_eval_obj_mem / 1e6,
+                        init_eval_obj_alloc = init_eval_obj_alloc,
+                        neval_grad = nlp.counters.neval_grad,
+                        init_eval_grad_time = init_eval_grad_time,
+                        init_eval_grad_mem = init_eval_grad_mem / 1e6,
+                        init_eval_grad_alloc = init_eval_grad_alloc,
+                        is_init_run = is_init_run,
+                    ),
                 )
+                is_init_run = false
+            catch e
+                @info "Solver failed on $(nlp.meta.name): $e"
+                break
             end
+            CSV.write(
+                filename,
+                DataFrame([last(df)]);
+                append = isfile(filename) && filesize(filename) > 0,
+            )
         end
     end
 end
